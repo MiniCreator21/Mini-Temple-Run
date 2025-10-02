4487e6dd823ac6d8dd0e85fcacc61f12dfee64a4
MiniCreator21 <minianogamer21@gmail.com>
2025-10-02 11:54:45 +0100

    added gradual speed increase decrease
    it makes sense i promise

--- Assets/Scripts/PlayerRunner.cs
+++ Assets/Scripts/PlayerRunner.cs
@@ -4,32 +4,54 @@ using UnityEngine.InputSystem;
 
 public class PlayerRunner : MonoBehaviour
 {
-    [SerializeField] private float forwardSpeed = 8f;
-    [SerializeField] private float speedIncrease = 0.01f;
+    private Rigidbody rb;
+    [SerializeField] private float speed = 8f;
+    [SerializeField] private float acceleration = 0.01f;
+    private float proximityToMaxSpeed;
+    [SerializeField] private float maxSpeed = 40f;
     [SerializeField] private float laneWidth = 2f;
     [SerializeField] int minLane = -1;
     [SerializeField] int maxLane = 1;
     [SerializeField] private float laneChangeSpeed = 12f;
     [SerializeField] private int currentLane = 0;
+
+    void Start()
+    {
+        proximityToMaxSpeed = (maxSpeed - speed) / maxSpeed;
+    }
+
     void Update()
     {
-        transform.Translate(Vector3.forward * forwardSpeed * Time.deltaTime);
-        forwardSpeed += speedIncrease;
-
         Vector3 pos = transform.position;
+        pos += Vector3.forward * speed * Time.deltaTime;
+        speed += acceleration * proximityToMaxSpeed;
+        proximityToMaxSpeed = (maxSpeed - speed) / maxSpeed;
+
         float targetX = currentLane * laneWidth;
         pos.x = Mathf.Lerp(pos.x, targetX, Time.deltaTime * laneChangeSpeed);
         transform.position = pos;
     }
 
-    void OnMoveLeft()
+    public void HandleMovement(InputAction.CallbackContext inputData)
     {
-        if (currentLane <= minLane) return;
-        currentLane -= 1;
+        Vector2 inputVector = inputData.ReadValue<Vector2>();
+
+        Debug.Log(inputVector.x);
+
+        if (inputVector.x != 0)
+        {
+            currentLane = Mathf.Clamp(currentLane + Mathf.RoundToInt(inputVector.x), minLane, maxLane);
+        }
     }
-    void OnMoveRight()
-    {
-        if (currentLane >= maxLane) return;
-        currentLane += 1;
-    }
+
+    //void OnMoveLeft()
+    //{
+    //    if (currentLane <= minLane) return;
+    //    currentLane -= 1;
+    //}
+    //void OnMoveRight()
+    //{
+    //    if (currentLane >= maxLane) return;
+    //    currentLane += 1;
+    //}
 }
